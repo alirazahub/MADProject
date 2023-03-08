09198902341b4dba9aaf33d07ed9e64b6debd0b0
alirazahb <aliraza.ar7785@gmail.com>
2023-03-08 21:40:57 +0500

    adding login screen

--- App.js
+++ App.js
@@ -1,63 +1,17 @@
 import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
-import image from './assets/icon.png'
+import { View } from 'react-native';
 import FlatListComp from './components/FlatListComp';
 
 export default function App() {
 
   return (
-    // <View style={{ flex: 1 }}>
-    //   <View style={{ flex: 0.30 }}>
-    //     <Image source={image} style={styles.image} />
-    //     <Text style={styles.loginText}>Login Here</Text>
-    //   </View>
-    //   <View style={{ flex: 0.50, marginTop: 100 }}>
-    //     <TextInput style={styles.input} placeholder="Email" />
-    //     <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" />
-
-    //   </View>
-    //   <View style={{ flex: 0.20 }}>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={{ textAlign: 'center' }}>Login</Text>
-    //     </TouchableOpacity>
-    //   </View>
-    // </View>
-    <View style={{flex:1}}>
-      <View style={{flex:0.30}}></View>
-      <View style={{flex:0.40}}>
-      <FlatListComp />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 0.30 }}></View>
+      <View style={{ flex: 0.40 }}>
+        <FlatListComp />
       </View>
-      <View style={{flex:0.30}}></View>
+      <View style={{ flex: 0.30 }}></View>
 
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  image: {
-    width: 100,
-    height: 100,
-    alignSelf: 'center',
-    marginTop: 20,
-    borderRadius: 50,
-  },
-  loginText: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginTop: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: 'blue',
-    margin: 10,
-    padding: 10,
-  },
-  button: {
-    borderWidth: 1,
-    borderColor: 'blue',
-    margin: 10,
-    padding: 10,
-
-  }
-});
---
+++ components/LoginScreen.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 0.30 }}>
+                <Image source={image} style={styles.image} />
+                <Text style={styles.loginText}>Login Here</Text>
+            </View>
+            <View style={{ flex: 0.50, marginTop: 100 }}>
+                <TextInput style={styles.input} placeholder="Email" />
+                <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" />
+
+            </View>
+            <View style={{ flex: 0.20 }}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={{ textAlign: 'center' }}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    image: {
+        width: 100,
+        height: 100,
+        alignSelf: 'center',
+        marginTop: 20,
+        borderRadius: 50,
+    },
+    loginText: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'blue',
+        margin: 10,
+        padding: 10,
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: 'blue',
+        margin: 10,
+        padding: 10,
+
+    }
+});
