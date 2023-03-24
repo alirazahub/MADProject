fd9f8d10d231083148bc9cf1e6197d5e061f47c9
alirazahb <aliraza.ar7785@gmail.com>
2023-03-24 12:45:24 +0500

    adding data to firebase

--- App.js
+++ App.js
@@ -1,87 +1,52 @@
-import React,{useState} from 'react';
-import { TouchableOpacity, View, Text, Button, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList, TextInput } from 'react-native';
 import FlatListComp from './components/FlatListComp';
 import SectionListComp from './components/SectionListComp';
-import useGetApi from './customHooks/GetApiCustomHook';
-import usePostApi from './customHooks/PostApiCustomHook';
+import { getDatabase, ref, onValue, set, push } from 'firebase/database';
+import app from './firebase-config.js';
+
+
+
 
 export default function App() {
-  const { data: getData, error: getError, loading: getLoading } = useGetApi('https://dummyjson.com/products');
-  const { data: postResponse, error: postError, loading: postLoading, postRequest } = usePostApi('https://dummyjson.com/products/add');
-  
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
-  const [stock, setStock] = useState('');
-  const [brand, setBrand] = useState('');
-  const handlePost = () => {
-    postRequest({
-      title,
-      description,
-      price,
-      stock,
-      brand,
+  const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
+  const addFood = () => {
+    setLoading(true);
+    const db = getDatabase(app);
+    const dbRef = ref(db, 'fastfood');
+
+    const newItemRef = push(dbRef);
+    set(newItemRef, {
+      key: 2,
+      title: title,
+      price: price,
+      description: description,
     });
+
+    setLoading(false);
   };
+
+
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 0.10 }}>
-
       </View>
       <View style={{ flex: 0.50 }}>
-        <Text>Post Data</Text>
-        <Text>Title</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setTitle(text)}
-          value={title}
-        />
-        <Text>Description</Text>
-        <TextInput
-
-style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setDescription(text)}
-          value={description}
-          />
-        <Text>Price</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setPrice(text)}
-          value={price}
-        />
-        <Text>Stock</Text>
-        <TextInput
-
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setStock(text)}
-          value={stock}
-        />
-        <Text>Brand</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setBrand(text)}
-          value={brand}
-        />
-        <Button
-          onPress={handlePost}
-          title="Post Data"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-          />
-        {postLoading && <Text>Posting data...</Text>}
-        {postError && <Text>Error posting data: {postError.message}</Text>}
-        {postResponse && <Text>Post response: {JSON.stringify(postResponse)}</Text>}
+        <Text style={{ fontSize: 30, fontWeight: "bold", color: 'black', textAlign: "center" }}>Add new Food</Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Title</Text>
+        <TextInput onChangeText={text => setTitle(text)} style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 7 }} />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Price</Text>
+        <TextInput onChangeText={text => setPrice(text)} style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 7 }} />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Description</Text>
+        <TextInput onChangeText={text => setDescription(text)} style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 7 }} />
+        {loading ? <Text>Loading...</Text> : null}
+        <Button onPress={addFood} title="Add" />
       </View>
       <View style={{ flex: 0.40 }}>
-        {
-          <FlatList
-            data={getData && getData.products}
-            renderItem={({ item }) => <View>
-              <Text>{item.title}</Text>
-            </View>}
-            keyExtractor={item => item.id}
-          />
-        }
+        <FlatListComp />
       </View>
 
     </View>
--- components/FlatListComp.js
+++ components/FlatListComp.js
@@ -1,52 +1,22 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { View, FlatList, Text, Image } from 'react-native'
+import {getDatabase,ref,onValue} from 'firebase/database';
+import app from '../firebase-config.js'
 
-const DATA = [
-  {
-    id: 1,
-    title: 'Morten Lava Burger',
-    rating: 5,
-    price: "$10",
-    image: require('../assets/img1.jpeg')
-  },
-  {
-    id: 2,
-    title: 'Molten Lava Burger',
-    rating: 5,
-    price: "$10",
-    image: require('../assets/img2.jpeg')
-  },
-  {
-    id: 3,
-    title: 'Crispy Chicken Burger',
-    rating: 5,
-    price: "$10",
-    image: require('../assets/img3.jpeg')
-  },
-  {
-    id: 4,
-    title: 'Grilled Chicken Burger',
-    rating: 5,
-    price: "$10",
-    image: require('../assets/img4.jpeg')
-  },
-  {
-    id: 5,
-    title: 'Simple Burger',
-    rating: 5,
-    price: "$10",
-    image: require('../assets/img2.jpeg')
-  },
-  {
-    id: 6,
-    title: 'New Simple Burger',
-    rating: 4.5,
-    price: "$20",
-    image: require('../assets/img3.jpeg')
-  },
-];
 
 const FlatListComp = () => {
+  const [DATA, setDATA] = useState();
+  useEffect(() => {
+    const db = getDatabase(app);
+    const dbRef = ref(db, 'fastfood');
+    console.log('Reciving data from firebase');
+    onValue(dbRef, (snapshot) => {
+      let data = snapshot.val();
+      let dataArr = Object.values(data);
+      setDATA(dataArr);
+      console.log('Data received from firebase');
+    });
+  },[]);
   return (
     <FlatList
       data={DATA}
@@ -56,13 +26,13 @@ const FlatListComp = () => {
           <View style={{ flex: 0.2 }}>
             <View style={{ flex: 1, flexDirection: 'row' }}>
               <Text style={{ flex: 0.50, fontSize: 20, fontWeight: "bold", color: 'yellow' }}>	&#10084;
-                <Text style={{ color: 'black', fontSize: 15 }}>{item.rating}</Text>
+                <Text style={{ color: 'black', fontSize: 15 }}></Text>
               </Text>
               <Text style={{ flex: 0.50, fontSize: 25, fontWeight: "bold", marginLeft: 115 }}>&#9734;</Text>
             </View>
           </View>
           <View style={{ flex: 0.6, width: 200 }}>
-            <Image source={item.image} style={{ width: 120, height: 120, alignSelf: "center" }} />
+            <Image source={{uri:item.image}} style={{ width: 120, height: 120, alignSelf: "center" }} />
           </View>
           <View style={{ flex: 0.2 }}>
             <View style={{ flex: 1, alignItems: "center" }}>
@@ -76,7 +46,7 @@ const FlatListComp = () => {
           </View>
         </View>
       )}
-      keyExtractor={item => item.id}
+      keyExtractor={item => item.key}
     />
   )
 }
